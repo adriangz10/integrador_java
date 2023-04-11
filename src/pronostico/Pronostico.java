6a7b2820ebd29096a36f4ca667c8ab02d1ebf985
adriangz10 <adrian-gomez@live.com>
2023-04-11 11:32:23 -0300

    Pequños cambios

--- src/pronostico/Pronostico.java
+++ src/pronostico/Pronostico.java
@@ -1,10 +1,10 @@
 package pronostico;
 
-public class Pronotico {
+public class Pronostico {
     private Partido partido;
     private Equipo equipo;
     private ResultadoEnum resultado;
-    public Pronotico(Partido partido, Equipo equipo, ResultadoEnum resultado) {
+    public Pronostico(Partido partido, Equipo equipo, ResultadoEnum resultado) {
         this.partido = partido;
         this.equipo = equipo;
         this.resultado = resultado;
